c6df5dec7732a6a6eab90de01c954d42043e61ba
Bergmans <d.bergmans@accenture.com>
2017-10-12 09:12:02 +0200

    Changes and added test files

--- angular-practice-REST/server.js
+++ angular-practice-REST/server.js
@@ -10,10 +10,14 @@ const port = 9000;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
   next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
